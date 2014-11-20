d7ca5f44fdb48575742813e5a34ed0209d98a570
raushan <raushan08bce253@yahoo.com>
2014-11-20 20:48:57 +0530

    missed files

---
+++ Queues/BaseQueue.cs
@@ -0,0 +1,53 @@
+﻿using SingleLinkedList;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    public class BaseQueue<T> where T : IComparable
+    {
+        node<T> front;
+        node<T> rear;
+
+
+        public void enqueue(T data)
+        {
+            node<T> temp = new node<T>() { data = data, Next = null };
+            if(rear==null)
+            {
+                rear = temp;
+                front = rear;
+            }
+            else
+            {
+                temp.Next = rear;
+                rear = temp;
+            }
+
+        }
+        public T dequeue()
+        {
+            T data;
+            if(front==null)
+            {
+                return default(T);
+            }
+            else
+            {
+                node<T> temp = rear;
+                while (temp.Next != front)
+                    temp = temp.Next;
+                data = temp.data;
+
+                front = temp;
+            }
+            return data;
+
+        }
+
+
+    }
+}
---
+++ SingleLinkedList/MergeSortList.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingleLinkedList
+{
+    public class MergeSortList<T> : baseList<T> where T : IComparable
+    {
+        public MergeSortList(baseList<T> obj)
+        {
+            this.Head = obj.Head;
+            this.count = obj.count;
+            this._current = base._current;
+        }
+
+
+        public baseList<T> sort(baseList<T> ip)
+        {
+
+        }
+        private void breakList(baseList<T> ip)
+        {
+
+        }
+
+        private baseList<T> merge(baseList<T> ip1, baseList<T> ip2)
+        {
+           
+            if (ip1 == null)
+                return ip2;
+            else if (ip2 == null)
+                return ip1;
+            else
+            {
+                if(ip1.Head.CompareTo(ip2.Head)>0)
+                {
+                    node<T> temp = ip1.Head;
+                    ip1.Head = ip2.Head;
+                    ip2.Head = temp;
+                    int cc = ip1.count;
+                    ip1.count = ip2.count;
+                    ip2.count = cc;
+                }
+                while(ip1!=null&&ip2!=null)
+                {
+                   
+                }
+            }
+        }
+
+
+
+        public void Display()
+        {
+            node<T> temp = new node<T>();
+            temp = this.Head;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.Next;
+            }
+        }
+    }
+}
